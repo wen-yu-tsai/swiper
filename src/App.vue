d5f8d81d224f97c73c6cffbc0bf6c2cfba94e1c3
Wenyu <Judy30058@gmail.com>
2025-05-15 14:39:13 +0800

    feat: swiper

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <!-- <div class="swiper-container"> -->
+    <swiper
+      v-if="slides.length > 0"
+      :slides-per-view="4"
+      :space-between="30"
+      :centered-slides="true"
+      :pagination="{ clickable: true }"
+      :modules="modules"
+      :breakpoints="{
+        640: { slidesPerView: 2, spaceBetween: 20 },
+        768: { slidesPerView: 3, spaceBetween: 25 },
+        1024: { slidesPerView: 4, spaceBetween: 30 }
+      }"
+      class="mySwiper"
+      @swiper="onSwiper"
+    >
+      <SlideItem
+          v-for="slide in slides"
+          :key="slide.id"
+          :slide-data="slide"
+        />
+        <!-- 不要寫錯了 -->
+    </swiper>
+  <!-- </div> -->
+</template>
+<script>
+  // Import Swiper Vue.js components
+  import { Swiper } from 'swiper/vue';
+  import SlideItem from "./components/SlideItem.vue"; //路徑要對，大小寫敏感
+
+  // Import Swiper styles
+  import 'swiper/css';
+
+  import 'swiper/css/pagination';
+
+  import './style.css';
+
+  // import required modules
+  import { Pagination } from 'swiper/modules';
+
+  export default {
+    name:"App",
+    components: {
+      Swiper,
+      SlideItem,
+    },
+    setup() {
+      //建立資料
+      const slides = [
+      { id: 1, imageUrl: 'https://picsum.photos/200/200?random=1', title: '圖片 1' },
+      { id: 2, imageUrl: 'https://picsum.photos/200/200?random=2', title: '圖片 2' },
+      { id: 3, imageUrl: 'https://picsum.photos/200/200?random=3', title: '圖片 3' },
+      { id: 4, imageUrl: 'https://picsum.photos/200/200?random=4', title: '圖片 4' },
+      { id: 5, imageUrl: 'https://picsum.photos/200/200?random=5', title: '圖片 5' },
+      { id: 6, imageUrl: 'https://picsum.photos/200/200?random=6', title: '圖片 6' },
+      { id: 7, imageUrl: 'https://picsum.photos/200/200?random=7', title: '圖片 7' },
+      { id: 8, imageUrl: 'https://picsum.photos/200/200?random=8', title: '圖片 8' },
+      { id: 9, imageUrl: 'https://picsum.photos/200/200?random=9', title: '圖片 9' },
+      { id: 10, imageUrl: 'https://picsum.photos/200/200?random=10', title: '圖片 10' },
+    ];
+
+    const onSwiper = (swiper) => {
+      console.log('Swiper initialized:', swiper);
+    };
+
+      return {
+        modules: [Pagination],
+        slides,//記得return 資料
+        onSwiper,
+      };
+    },
+  };
+</script>
---
+++ src/components/SlideItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <swiper-slide class="slide-item">
+    <img :src="slideData.imageUrl" :alt="slideData.title" class="slide-image" />
+    <h3>{{ slideData.title }}</h3>
+  </swiper-slide>
+</template>
+
+<script>
+import { SwiperSlide } from 'swiper/vue';
+
+export default {
+  name: 'SlideItem',
+  components: {
+    SwiperSlide,
+  },
+  props: {
+    slideData: {
+      type: Object,
+      required: true,
+      default: () => ({
+        imageUrl: '',
+        title: '',
+      }),
+    },
+  },
+};
+</script>
+
+<style scoped>
+.slide-item {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  /* width: 200px*/
+  height: 200px;
+  min-width:200px;
+  border: 1px solid purple; /* 診斷用邊框 */
+}
+
+.slide-image {
+  width: 100%;
+  height: 80%;
+  object-fit: cover;
+  border:1px solid black
+}
+
+h3 {
+  margin: 10px 0 0;
+  font-size: 16px;
+  color:black
+}
+</style>
